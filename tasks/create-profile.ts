a0f79eac0daf020ff82fca000fc53b426e55e90c
Carlos Beltran <imthatcarlos@gmail.com>
2022-03-26 15:17:21 -0400

    push all tasks

---
+++ tasks/create-profile.ts
@@ -0,0 +1,29 @@
+import { task } from 'hardhat/config';
+import { LensHub__factory } from '../typechain-types';
+import { CreateProfileDataStruct } from '../typechain-types/LensHub';
+import { waitForTx, initEnv, getAddrs, ZERO_ADDRESS } from './helpers/utils';
+
+task('create-profile', 'creates a profile').setAction(async ({}, hre) => {
+  const [governance, , user] = await initEnv(hre);
+  const addrs = getAddrs();
+  const lensHub = LensHub__factory.connect(addrs['lensHub proxy'], governance);
+
+  await waitForTx(lensHub.whitelistProfileCreator(user.address, true));
+
+  const inputStruct: CreateProfileDataStruct = {
+    to: user.address,
+    handle: '0xcarlos',
+    imageURI: 'ipfs://QmZFnUm3bjSyEPrvxEa3fR9eUxnkfQeLmPTzDhAmCWtbMZ/3958.png',
+    followModule: ZERO_ADDRESS,
+    followModuleData: [],
+    followNFTURI: 'ipfs://QmWGAFtzyzB6A6gYMnb6838hysHuT2rcV8B98Gmj4T4pyY/3958.json',
+  };
+
+  await waitForTx(lensHub.connect(user).createProfile(inputStruct));
+
+  console.log(`Total supply (should be 1): ${await lensHub.totalSupply()}`);
+  console.log(
+    `Profile owner: ${await lensHub.ownerOf(1)}, user address (should be the same): ${user.address}`
+  );
+  console.log(`Profile ID by handle: ${await lensHub.getProfileIdByHandle('zer0dot')}`);
+});
