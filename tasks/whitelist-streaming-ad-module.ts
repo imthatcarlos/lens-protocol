99eac258dd87f84a1e3a50d1a8b2b9021b327404
Carlos Beltran <imthatcarlos@gmail.com>
2022-04-23 22:09:27 -0400

    whitelist streaming ad module

---
+++ tasks/whitelist-streaming-ad-module.ts
@@ -0,0 +1,35 @@
+import '@nomiclabs/hardhat-ethers';
+import { hexlify, keccak256, RLP } from 'ethers/lib/utils';
+import fs from 'fs';
+import { task } from 'hardhat/config';
+import {
+  LensHub__factory,
+} from '../typechain-types';
+import { deployContract, waitForTx, getAddrs } from './helpers/utils';
+
+task('whitelist-streaming-ad-module', 'whitelists the new reference module StreamingAdModule from madfi').setAction(async ({}, hre) => {
+  // Note that the use of these signers is a placeholder and is not meant to be used in
+  // production.
+  const ethers = hre.ethers;
+  const networkName = hre.network.name;
+  const accounts = await ethers.getSigners();
+  const deployer = accounts[0];
+  const governance = accounts[1];
+  const addrs = getAddrs();
+
+  // Nonce management in case of deployment issues
+  let deployerNonce = await ethers.provider.getTransactionCount(deployer.address);
+
+  const hubProxyAddress = addrs['lensHub proxy'];
+
+  // AFTER running `yarn deploy:docker` in other repo
+  const MODULE_ADDRESS = addrs['StreamingAdModule'];
+  if (!MODULE_ADDRESS) throw new Error ('need to define MODULE_ADDRESS');
+
+  // Whitelist the reference module
+  const lensHub = LensHub__factory.connect(hubProxyAddress, governance);
+  console.log(`\n\t-- Whitelisting Reference Module (at: ${MODULE_ADDRESS}) --`);
+  await waitForTx(
+    lensHub.whitelistReferenceModule(MODULE_ADDRESS, true)
+  );
+});
